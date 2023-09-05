6a710b65dea648e34061d25f5d85b3bc026a2df7
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-05 05:47:22 +0200

    feat: add function to update the position and style of an element
    Add a function that takes an object as an argument that contains the elementToUpdate, referenceElement, and containerElement properties. The function updates the position and style of the elementToUpdate based on the referenceElement and the containerElement.

---
+++ src/utils/updateElementPosition.ts
@@ -0,0 +1,44 @@
+interface UpdateElementPositionParams {
+    elementToUpdate: HTMLElement;
+    referenceElement: HTMLElement;
+    containerElement?: HTMLElement;
+  }
+  
+  const updateElementPosition = ({
+    elementToUpdate,
+    referenceElement,
+    containerElement,
+  }: UpdateElementPositionParams) => {
+    let scrollLeft = 0;
+    let scrollTop = 0;
+    let containerPosition;
+    const referenceElementPosition = referenceElement.getBoundingClientRect();
+  
+    if (containerElement) {
+      scrollLeft = containerElement.scrollLeft;
+      scrollTop = containerElement.scrollTop;
+      containerPosition = containerElement.getBoundingClientRect();
+    } else {
+      const body = document.getElementsByTagName("body")[0];
+      scrollLeft = window.scrollX;
+      scrollTop = window.scrollY;
+      containerPosition = body.getBoundingClientRect();
+    }
+  
+    elementToUpdate.style.width = `${referenceElementPosition.width}px`;
+    elementToUpdate.style.height = `${referenceElementPosition.height}px`;
+    elementToUpdate.style.left = `${
+      referenceElementPosition.left + scrollLeft
+    }px`;
+    elementToUpdate.style.right = `${referenceElementPosition.right}px`;
+    elementToUpdate.style.top = `${
+      referenceElementPosition.top - containerPosition.top + scrollTop
+    }px`;
+    elementToUpdate.style.bottom = `${
+      referenceElementPosition.bottom - containerPosition.top
+    }px`;
+    elementToUpdate.style.borderRadius =
+      window.getComputedStyle(referenceElement).borderRadius;
+  };
+
+  export default updateElementPosition
