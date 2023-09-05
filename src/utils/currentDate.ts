ca3665d27d95c1ebb1957061c7014e8db3388e4c
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-05 05:43:29 +0200

    feat(currentDate): add timeStamp function
    Add variables and functions that get the current date and time in different formats.

---
+++ src/utils/currentDate.ts
@@ -0,0 +1,21 @@
+export const today = new Date();
+export const day = today.getDate();
+export const year = today.getFullYear();
+export const month = today.getMonth();
+// const currentDate = `${year}-${month + 1}-${day}`;
+
+export function timeStamp(locale: string) {
+    const timeStamp = new Date(Date.now());
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    return timeStamp.toLocaleString(locale, options);
+  }
+
+// export default currentDate;
