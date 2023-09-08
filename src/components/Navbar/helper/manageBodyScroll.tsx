a4dee31a0cc3f736c21324d4493eb0530c59bdce
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-08 11:12:01 +0200

    feat(Navbar/helper): add manageBodyScroll function
    Create manageBodyScroll function to be used to toggle the overflow property of the document.body element. This is useful for preventing the user from scrolling the page when a popover or other modal is open.

---
+++ src/components/Navbar/helper/manageBodyScroll.tsx
@@ -0,0 +1,12 @@
+export const manageBodyScroll = (condition: boolean) => {
+    if (condition) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  };
+
