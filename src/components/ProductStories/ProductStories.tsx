b3bfeca975477d2cfa6c384dd611e5b954235668
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-11 08:20:44 +0200

    feat(components): Add ProductStories component
    This commit introduces a ProductStories component that displays a slider of product categories, each category having its own set of stories. The slider uses the KeenSlider library for slide transitions and 'react-insta-stories' library for story presentation.
    
    The ProductStories component uses a custom hook, 'useFetchProducts', to fetch products and categories data from a provided URL. Each category is displayed as a slide in the slider, and the stories within each category are displayed in an Instagram-like stories format.
    
    The ProductStories component also includes a dialog feature. When a category slide is clicked, a dialog is opened to display the stories of that category in a full-screen view. The dialog can be closed by clicking a close button or when all stories of the category have been viewed.

---
+++ src/components/ProductStories/ProductStories.tsx
@@ -0,0 +1,168 @@
+import { useContext, useEffect, useMemo, useState } from "react";
+import ProductContext from "@/contexts/ProductsContext/ProductsContext";
+import KeenSlider from "@/contexts/KeenSliderContext/KeenSlider";
+import { organizeData } from "@/utils/groupByCategory";
+import { storiesCards } from "@/components/Cards/helpers/storiesCards";
+import { avatarsCreator } from "@/components/Avatar/helper/avatars";
+import UnorderedList from "../UnorderedList/UnorderedList";
+import * as Portal from "@radix-ui/react-portal";
+import { Close } from "../Icons";
+import { QueryResponse } from "@/types/QueryResponse";
+import { Products } from "@/types/Product";
+import { Categories, Category } from "@/types/Categories";
+import { ConfigItems } from "@/types/ConfigItems";
+import { compareItems } from "@/utils/compare";
+import StoriesSlider from "@/components/keenSlider/StoriesSlider";
+import { useRouter } from "next/router";
+import { useFetchProducts } from "@/hooks/useFetchProducts";
+
+interface ProductStoriesProps {
+  query: QueryResponse;
+}
+interface TriggerProps {
+  slideIndex: number;
+  handleStoriesDialog: (isOpen: boolean, slideIndex?: number) => void;
+  children: React.ReactNode;
+  className?: string;
+  canOpenDialog?: boolean;
+}
+
+const ProductStories: React.FunctionComponent<ProductStoriesProps> = ({
+  query,
+}) => {
+  const url = "https://dummyjson.com/products";
+  const { categories, products} = useFetchProducts(url)
+  // const [products, setProducts] = useState<Products>([]);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const router = useRouter();
+
+  const handleDialog = (isOpen: boolean) => {
+    setDialogOpen(isOpen);
+  };
+  const handleStoriesDialog = (isOpen: boolean, slideIndex?: number) => {
+    if (slideIndex !== undefined) {
+      setCurrentSlide(slideIndex);
+    }
+
+    setDialogOpen(isOpen);
+  };
+
+  categories.sort((a, b) =>
+    a.title === "All products" ? -1 : b.title === "All products" ? 1 : 0
+  );
+
+  // useEffect(() => {
+  //   if (query.data !== undefined) {
+  //     setProducts(query.data.products);
+  //   }
+  // }, [products, query.data]);
+
+  useEffect(() => {
+    setDialogOpen(false);
+  }, [router.asPath]);
+
+  const triggers = useMemo(() => {
+    const items = JSON.parse(JSON.stringify(categories)).filter(
+      (category: Category<Products>) => category.title !== "All products"
+    );
+
+    const avatars = avatarsCreator({
+      data: items,
+      itemNumber: 0,
+    });
+
+    return avatars.map((item, index) => {
+      return {
+        label: item.label,
+        component: (
+          <>
+            <Trigger
+              slideIndex={index}
+              handleStoriesDialog={handleStoriesDialog}
+              canOpenDialog={true}
+            >
+              {item.component}
+            </Trigger>
+          </>
+        ),
+      };
+    });
+  }, [categories]);
+
+  const stories = useMemo(() => {
+    const items: Categories<Products> = JSON.parse(
+      JSON.stringify(categories)
+    ).filter(
+      (category: Category<Products>) => category.title !== "All products"
+    );
+
+    return items.map((item, index) => {
+      const storiesConfig = storiesCards({
+        data: item.items,
+        itemNumber: 2,
+      });
+
+      const category = {
+        ...item,
+        items: storiesConfig,
+      };
+
+      return category;
+    });
+  }, [categories]);
+
+  return (
+    <>
+      <UnorderedList
+        listClassName="md:flex sm:flex gap-6 overflow-scroll w-full px-7 py-4 scrollbar-hide bg-gray-100 md:justify-center sm:justify-start lg:hidden"
+        itemClassName="text-center"
+        items={triggers}
+      />
+      {dialogOpen && (
+        <Portal.Root className="fixed w-full h-full z-[9999] top-0">
+          <div className="bg-black  w-full">
+            <Trigger
+              className="w-4 h-4 absolute top-0 right-0 m-6 z-[9999]"
+              handleStoriesDialog={handleStoriesDialog}
+              slideIndex={0}
+            >
+              <Close className="stoke-white opacity-70" />
+            </Trigger>
+            <div className="w-full h-full">
+              <KeenSlider initialSlide={currentSlide}>
+                <StoriesSlider
+                  items={stories}
+                  listClassName="w-full h-screen"
+                  handleDialog={handleStoriesDialog}
+                />
+              </KeenSlider>
+            </div>
+          </div>
+        </Portal.Root>
+      )}
+    </>
+  );
+};
+
+const Trigger: React.FunctionComponent<TriggerProps> = ({
+  children,
+  slideIndex,
+  handleStoriesDialog,
+  className,
+  canOpenDialog,
+}) => {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        handleStoriesDialog(canOpenDialog || false, slideIndex);
+      }}
+      className={className}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ProductStories;
