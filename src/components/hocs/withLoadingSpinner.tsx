2b15147cbef9359d39a72fcf5ff55061f092c94f
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-11 07:50:29 +0200

    feat(hoc): Add HOC for async image loading with spinner
    This commit introduces a Higher Order Component (HOC) named 'withLoadingSpinner'. This HOC takes a component (specifically an image component) and returns a new component that shows a loading spinner until the image within the component is fully loaded.
    
    This HOC can improve the user experience by providing a loading indicator.

---
+++ src/components/hocs/withLoadingSpinner.tsx
@@ -0,0 +1,27 @@
+import { Spinner } from "@/components/Icons";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import React, { useState } from "react";
+import Image from "next/image";
+
+interface LoadingProps extends React.PropsWithChildren<React.ComponentProps<typeof Image>> {}
+
+interface ComponentProps<T> {
+  Component: React.ComponentType<T>;
+}
+
+export const withLoadingSpinner = (
+  Component: React.ComponentType<LoadingProps>
+) => {
+  return ({ ...props }: Omit<LoadingProps, keyof ComponentProps<LoadingProps>>) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+    return (
+      <div className="relative w-full h-full">
+        {!imageLoaded && <LoadingSpinner />}
+        <Component
+          {...props}
+          onLoad={() => setImageLoaded(true)}
+        />
+      </div>
+    );
+  };
+};
