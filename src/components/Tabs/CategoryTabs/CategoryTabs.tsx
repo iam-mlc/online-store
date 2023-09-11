d09f4ffddd0982509bbc1af325c721263bc258f6
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-11 08:37:43 +0200

    feat(components): Implement CategoryTabs component
    This commit introduces a CategoryTabs component that displays product categories in tabs which contain a slider. The slider of each category displays products based on the corresponding category.
    
    In addition, the CategoryTabs component includes a loading state that is displayed until the product data is fetched. The loading state is implemented using a Loading component that displays placeholder data.

---
+++ src/components/Tabs/CategoryTabs/CategoryTabs.tsx
@@ -0,0 +1,185 @@
+import * as RadixTabs from "@radix-ui/react-tabs";
+import { organizeData } from "@/utils/groupByCategory";
+import { useEffect, useMemo, useState } from "react";
+import { avatarsCreator } from "@/components/Avatar/helper/avatars";
+import { productCards } from "@/components/Cards/helpers/productCards";
+// import { sliders } from "@/components/swiperSlider/helpers/sliders";
+import KeenSlider from "@/contexts/KeenSliderContext/KeenSlider";
+import TabsSlider from "@/components/keenSlider/TabsSlider";
+import { Products } from "@/types/Product";
+import { Categories } from "@/types/Categories";
+import Tabs from "../Tabs";
+import { ListItem } from "@/types/ListItem";
+import { TriggerAndContent } from "@/types/TriggerAndContent";
+import { QueryResponse } from "@/types/QueryResponse";
+import { useWindowSize } from "react-use";
+
+interface ITabsProps {
+
+  data: Categories<Products> | undefined;
+}
+
+const CategoryTabs: React.FunctionComponent<ITabsProps> = ({ data }) => {
+  const [items, setItems] = useState<Categories<Products>>([]);
+
+  useEffect(() => {
+    if (data) {
+      const dataItems = data.filter((item) => {
+        return item.title !== "All products";
+      });
+      setItems(dataItems);
+    }
+  }, [data]);
+
+  const { width, height } = useWindowSize();
+  const breakpoits = {
+    lg: 1024,
+    md: 768,
+    sm: 480,
+    xsm: 100,
+  };
+
+  function handleBreakpoits() {
+    if (width > breakpoits.lg) {
+      return 1.7;
+    }
+    if (width > breakpoits.md) {
+      return 1.9;
+    }
+    if (width > breakpoits.sm) {
+      return 1.2;
+    }
+    if (width > breakpoits.xsm) {
+      return 1.2;
+    }
+  }
+
+  const tabsItems: TriggerAndContent[] = useMemo(() => {
+    // const items: Categories<Products> = JSON.parse(JSON.stringify(data));
+
+    const avatars = avatarsCreator({
+      data: items,
+      itemNumber: 1,
+    });
+
+    const tabs = items.map((item, index) => {
+      const avatar = avatars[index];
+
+      const slider: ListItem = {
+        label: item.title + index,
+        component: (
+          <>
+            <KeenSlider
+              slidesPerView={handleBreakpoits()}
+              loop={true}
+              origin={"center"}
+            >
+              <TabsSlider items={item.items} />
+            </KeenSlider>
+          </>
+        ),
+      };
+
+      return {
+        trigger: avatar,
+        content: slider,
+      };
+    });
+
+    return tabs;
+  }, [items]);
+
+  if (data && data.length === 0 && items.length === 0) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Tabs
+        items={tabsItems}
+        listClassName="flex flex-row gap-4 px-8 sm:overflow-x-scroll lg:overflow-hidden md:justify-center sm:justify-start scrollbar-hide max-[768px]:pl-8  max-[990px]:pl-[8em]"
+        triggerClassName="rounded-full p-4 border-black h-[8em] min-w-[8em] block relative overflow-hidden"
+        className="flex flex-col gap-4"
+        hasVisualEffect={true}
+      />
+    </>
+  );
+};
+
+const Loading = () => {
+  const placeholders = Array(6).fill({});
+
+  const { width, height } = useWindowSize();
+  const breakpoits = {
+    lg: 1024,
+    md: 768,
+    sm: 480,
+    xsm: 200,
+  };
+
+  function handleBreakpoits() {
+    if (width > breakpoits.lg) {
+      return 1.7;
+    }
+    if (width > breakpoits.md) {
+      return 1.9;
+    }
+    if (width > breakpoits.sm) {
+      return 1.8;
+    }
+    if (width > breakpoits.xsm) {
+      return 1.2;
+    }
+  }
+
+  const tabsItems: TriggerAndContent[] = (function () {
+    const items: Categories<Products> = JSON.parse(
+      JSON.stringify(placeholders)
+    );
+
+    const avatars = avatarsCreator({
+      data: items,
+      itemNumber: 1,
+    });
+
+    const tabs = items.map((item, index) => {
+      const avatar = avatars[index];
+
+      const slider: ListItem = {
+        label: "Placeholder" + index,
+        component: (
+          <>
+            <KeenSlider
+              slidesPerView={handleBreakpoits()}
+              loop={true}
+              origin={"center"}
+            >
+              <TabsSlider items={items} />
+            </KeenSlider>
+          </>
+        ),
+      };
+
+      return {
+        trigger: avatar,
+        content: slider,
+      };
+    });
+
+    return tabs;
+  })();
+
+  return (
+    <>
+      <Tabs
+        items={tabsItems}
+        listClassName="flex flex-row gap-4 px-8 sm:overflow-x-scroll lg:overflow-hidden md:justify-center sm:justify-start scrollbar-hide max-[768px]:pl-8  max-[990px]:pl-[8em]"
+        triggerClassName="rounded-full p-4 border-black h-[8em] min-w-[8em] block relative overflow-hidden"
+        className="flex flex-col gap-4"
+        hasVisualEffect={true}
+      />
+    </>
+  );
+};
+
+export default CategoryTabs;
