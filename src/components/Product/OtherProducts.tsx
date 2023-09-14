f1904660497e99f68b7ebd4b6727c92354d667b3
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-14 08:25:38 +0200

    feat(components): Add OtherProducts component
    This commit adds a new component called OtherProducts that displays other products that the user might be interested in. The component takes a Product object as a prop and returns two product banners, one displaying products from the same category as the current product and one displaying products from other categories.

---
+++ src/components/Product/OtherProducts.tsx
@@ -0,0 +1,103 @@
+import { useFetchAndQuery } from "@/hooks/useFetchAndQuery";
+import { Product, Products } from "@/types/Product";
+import { QueryResponse } from "@/types/QueryResponse";
+import dynamic from "next/dynamic";
+import { useCallback, useEffect, useMemo, useState } from "react";
+// import FeaturedProduct from "@/components/FeaturedProducts/FeaturedProducts";
+import ProductsBanner from "../ProductsBanner";
+import { useFetchProducts } from "@/hooks/useFetchProducts";
+import { useRouter } from "next/router";
+import shuffleArray from "@/utils/shuffleArary";
+
+const FeaturedProduct = dynamic(
+  () => import("@/components/FeaturedProducts/FeaturedProducts"),
+  {
+    ssr: false,
+  }
+);
+
+interface OtherProductsProps {
+  data: Product;
+}
+const otherProductsDetails = [
+  {
+    title: `Take a look at these other products we prepared for you `,
+  },
+  {
+    title: `We have more stuff you might like !`,
+  },
+  {
+    title: `We have great products at a great price !`,
+  },
+  {
+    title: `We think you might like these other products`,
+  },
+];
+
+const randomNumber = Math.floor(Math.random() * otherProductsDetails.length);
+
+const OtherProducts: React.FC<OtherProductsProps> = ({ data }) => {
+  const url = "https://dummyjson.com/products";
+  const { getCategory, products } = useFetchProducts(url);
+
+  const router = useRouter();
+
+  const category = getCategory(data.category);
+
+  const excludeCurrentProduct = useCallback(
+    (items: Products) => {
+      return items.filter((item) => item.id !== data.id).slice(0, 8);
+    },
+    [data]
+  );
+
+  const selectRandomProducts = useCallback(
+    (items: Products) => {
+      const products = shuffleArray(items);
+
+      return products
+        .filter(
+          (item) => item.category !== data.category && item.id !== data.id
+        )
+        .slice(0, 8);
+    },
+    [data]
+  );
+
+  const sameCategoryCopywrite = [
+    {
+      title: `Take a look at these other ${data.category} `,
+    },
+    {
+      title: `We have more ${data.category} just for you`,
+    },
+    {
+      title: `Here are some other ${data.category}`,
+    },
+    {
+      title: `We think you might enjoy these other ${data.category}`,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-28">
+      <div>
+        <ProductsBanner
+          copywriteMessage={sameCategoryCopywrite[randomNumber]}
+          isReversed={true}
+          data={category ? excludeCurrentProduct(category.items) : undefined}
+          key={router.asPath}
+        />
+      </div>
+      <div>
+        <ProductsBanner
+          copywriteMessage={otherProductsDetails[randomNumber]}
+          data={selectRandomProducts(products)}
+          key={router.asPath}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default OtherProducts;
