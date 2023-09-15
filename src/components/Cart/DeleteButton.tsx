0ad710c548b78c0feddf8b91934d87b6568b6ad2
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-15 07:15:02 +0200

    feat(components): add DeleteButton component for cart items
    Implement a DeleteButton component that allows users to remove items from the cart.

---
+++ src/components/Cart/DeleteButton.tsx
@@ -0,0 +1,20 @@
+import { Delete } from "@/components/Icons";
+import { CartContext } from "@/contexts/CartContext/cartContext";
+import { ProductInCart, ProductsInCart } from "@/types/Product";
+import { useContext } from "react";
+
+interface DeleteButton {
+  data: ProductInCart;
+}
+const DeleteButton: React.FC<DeleteButton> = ({ data }) => {
+  const { removeFromCart } = useContext(CartContext);
+  return (
+    <>
+      <button onClick={() => removeFromCart(data.id)}>
+        <Delete className="md:w-5 md:h-5 w-4 h-4" />
+      </button>
+    </>
+  );
+};
+
+export default DeleteButton;
