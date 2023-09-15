e2b127eeef82bdaefa5f8f5cdd92c5d8d18aaec7
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-15 07:11:45 +0200

    feat(components): add Breadcrumb component
    Implement a Breadcrumb component that represents the user's navigation path within the application.

---
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -0,0 +1,68 @@
+import { useRouter } from "next/router";
+import * as React from "react";
+import OrderedList from "../OrderedList/OrderedList";
+import Link from "next/link";
+import { ArrowRight, CaretRight, RightAngle } from "../Icons";
+
+interface BreadcrumbStep {
+  label: string;
+  path: string;
+}
+
+interface BreadcrumbProps {
+  pathway: BreadcrumbStep[];
+}
+
+const Breadcrumb: React.FunctionComponent<BreadcrumbProps> = ({ pathway }) => {
+  const router = useRouter();
+  const currentPath = router.asPath;
+
+  const getCurrentStepIndex = () => {
+    return pathway.findIndex((step) => step.path === currentPath);
+  };
+
+  const items = pathway.map((step, index) => {
+    return {
+      label: step.path,
+      component: (
+        <div className="flex gap-1 items-center ">
+          <div className="relative  ">
+            {index === getCurrentStepIndex() ? (
+              <>
+                <span className="text-xs font-medium inline-block underline underline-offset-4">{step.label}</span>
+                {/* <div className="absolute top-0 right-0 bg-black/[.10] w-full h-full"></div> */}
+              </>
+            ) : (
+              <Link
+                href={step.path}
+                className="text-xs font-medium inline-block"
+              >
+                {step.label}
+              </Link>
+            )}
+          </div>
+            {
+              !(index === pathway.length - 1) && (
+                <div>
+                  {/* <RightAngle className="w-3 h-3"/> */}
+                  <span className="font-bold">&gt;</span>
+                </div>
+              )
+            }
+        </div>
+      ),
+    };
+  });
+
+  return (
+    <nav aria-label="breadcrumb" className="flex">
+      <OrderedList
+        items={items}
+        itemClassName="flex items-center"
+        listClassName="flex overflow-hidden text-gray-600 gap-1"
+      />
+    </nav>
+  );
+};
+
+export default Breadcrumb;
