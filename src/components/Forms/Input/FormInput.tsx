2e6459d8db4368d79c6db2ec324bb0354836316e
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-15 08:25:15 +0200

    feat(components): add a FormInput component
    Add a FormInput component that renders a label, a field, and an error message. It leverages the formik library to add validation

---
+++ src/components/Forms/Input/FormInput.tsx
@@ -0,0 +1,51 @@
+import toCamelCase from "@/utils/toCamelCase";
+import { RenderProps } from "../CheckoutForm";
+import { ErrorMessage, Field } from "formik";
+import checkoutSchema from "../helpers/validationSchema/schema";
+import isFieldRequired from "../helpers/isFieldRequired";
+
+interface FormInputProps {
+  label: string;
+  hasPlaceholder?: boolean;
+  receivesComponent: React.FC<RenderProps>;
+  handleErrorMessage: (msg: string) => JSX.Element;
+  hasNote?: string;
+  // isOptional: boolean;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+  label,
+  hasPlaceholder,
+  receivesComponent,
+  handleErrorMessage,
+  hasNote,
+  // isOptional = true,
+}) => {
+  const fieldName = toCamelCase(label);
+  const isRequired = isFieldRequired(fieldName, checkoutSchema);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={fieldName} className="ml-2">
+        <span>{label}</span>
+        {isRequired && <span className="text-red-500">*</span>}
+        <span className="text-[0.8em] text-black/[.85] ml-2">{hasNote}</span>
+      </label>
+      <Field
+        id={fieldName}
+        name={fieldName}
+        placeholder={
+          hasPlaceholder
+            ? `${label} ${isRequired ? "(Required) " : "(Optional)"}`
+            : ""
+        }
+        component={receivesComponent || undefined}
+      />
+      <div className="ml-2">
+        <ErrorMessage name={fieldName} render={handleErrorMessage} />
+      </div>
+    </div>
+  );
+};
+
+export default FormInput;
