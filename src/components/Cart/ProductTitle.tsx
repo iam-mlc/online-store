c916a0fe64f8279cd503dfb77b4dad68b6f73000
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-15 07:09:52 +0200

    feat(components): add ProductTitle component
    Implement a ProductTitle component that displays the title and thumbnail of a product in the cart

---
+++ src/components/Cart/ProductTitle.tsx
@@ -0,0 +1,30 @@
+import { withLoadingSpinner } from "@/components/hocs/withLoadingSpinner";
+import { ProductInCart, ProductsInCart } from "@/types/Product";
+import NextImage from "next/image";
+
+const Image = withLoadingSpinner(NextImage);
+
+interface ProductTitle {
+  data: ProductInCart;
+}
+
+const ProductTitle: React.FC<ProductTitle> = ({ data }) => {
+  return (
+    <div>
+      <div className="flex gap-4  items-center w-full sm:justify-center md:justify-start">
+        <div className="md:w-[7em] md:h-[6em] w-[5em] h-[3em] rounded-[1em] overflow-hidden sm:hidden md:block">
+          <Image
+            src={data.thumbnail}
+            alt={data.title}
+            width={100}
+            height={100}
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="font-bold md:text-lg ">{data.title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductTitle;
