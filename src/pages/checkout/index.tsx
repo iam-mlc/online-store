d7cabd82c88eaf212f4d6263f8ba97eec5229c9f
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-17 09:18:25 +0200

    feat: Add checkout page

---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,97 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import CheckoutForm from "@/components/Forms/CheckoutForm";
+import Table from "@/components/Table/Table";
+import { tables } from "@/components/Table/helpers/table";
+import UnorderedList from "@/components/UnorderedList/UnorderedList";
+import { CartContextValues } from "@/contexts/CartContext/CartProvider";
+import { CartContext } from "@/contexts/CartContext/cartContext";
+import GeolocationContext from "@/contexts/GeoLocationContext/GeolocationContext";
+import LocaleContext from "@/contexts/LocaleContext/LocaleContext";
+import OrderSummaryContext from "@/contexts/OrderContext/OrderSummaryContext";
+import { useDeliveryCalculator } from "@/hooks/useDeliveryCalculator";
+import { Coordinates } from "@/types/Coordinates";
+import { Product, ProductInCart } from "@/types/Product";
+import { UserLocation } from "@/types/userLocation";
+import { calculateDistanceCost } from "@/utils/calculateDistanceCost";
+import { Separator } from "@ariakit/react";
+import { getDistance } from "geolib";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+
+interface ICheckoutProps {}
+// const LOCAL_CURRENCY = "MT";
+// const ISO_CURRENCY = "MZN";
+const pathway = [
+  { label: "Cart", path: "/cart" },
+  { label: "Checkout", path: "/checkout" },
+];
+
+const Checkout: React.FunctionComponent<ICheckoutProps> = ({}) => {
+  const { order } = useContext(OrderSummaryContext);
+
+  const { STORE_LOCATION, coords } = useContext(GeolocationContext);
+
+  const { numberToCurrency } = useContext(LocaleContext);
+
+  const productsSubTotal = numberToCurrency(
+    order.subTotals.totalItems.withoutDiscount
+  );
+  const deliverySubTotal = numberToCurrency(
+    order.quantity >= 3 ? order.subTotals.delivery : 0
+  );
+
+  const total = numberToCurrency(order.total.withoutDiscount);
+
+  if (order.quantity === 0) {
+    return (
+      <main>
+        <div className="p-8 w-full text-center flex flex-col gap-12 justify-center items-center">
+          <h1 className="  text-xl md:text-3xl lg:text-5xl font-bold">
+            You don't have any products in the cart yet. 🤔
+          </h1>
+          <Link
+            href={"/products"}
+            className="w-fit font-bold text-white  py-2 px-8 rounded-full inline-block bg-black/[.40] hover:bg-black/[.60] text-center "
+          >
+            Start Shopping
+          </Link>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <section>
+        <div className="flex flex-col gap-4 ml-4">
+          <Breadcrumb pathway={pathway} />
+          {/* <Link href={"/cart"} className="underline underline-offset-4 mb-12">&lt;&lt; Back to Cart</Link> */}
+        </div>
+      </section>
+      <div className="flex md:flex-row flex-col-reverse md:py-16">
+        <section className="lg:w-[50%] w-full py-20 md:py-0">
+          <CheckoutForm />
+        </section>
+        <section className="lg:w-[50%] w-full h-full">
+          <div className="md:fixed right-0 md:rounded-l-full md:rounded-br-none w-full md:w-[50%] p-8  md:p-12 bg-black/[.80] text-white  md:h-fit flex flex-col gap-2 justify-center">
+            <div className="flex justify-between">
+              <span className="opacity-40">Products</span>
+              <span>{productsSubTotal}</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="opacity-40">Delivery</span>
+              <span>{deliverySubTotal}</span>
+            </div>
+            <Separator className="bg-white w-full h-[0.20em] my-2" />
+            <div className="flex justify-between font-bold text-xl">
+              <span className="">TOTAL</span>
+              <span>{total}</span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Checkout;
